77c50f395c6cbb862d00c7f4946d86bbb7a49b72
Nurgulll <93878260+Nurgulll@users.noreply.github.com>
2021-11-17 21:12:54 +0100

    Update Client.java

--- src/com/Riddles_in_Dark/Client.java
+++ src/com/Riddles_in_Dark/Client.java
@@ -19,7 +19,7 @@ public class Client {
      */
     public void login() {
         try {
-            ArrayList<String> user = getLoginOrRegisterDetails();
+            ArrayList<String> user = LoginOrRegisterInfo();
             Request loginRequest = new Request(user.get(0),user.get(1), "login");
             ClientOutputThread clientOutputThread = new ClientOutputThread(connection, loginRequest);
             Thread thread = new Thread(clientOutputThread);
@@ -34,7 +34,7 @@ public class Client {
      */
     public void register() {
         try {
-            ArrayList<String> user = getLoginOrRegisterDetails();
+            ArrayList<String> user = LoginOrRegisterInfo();
             Request registerRequest = new Request(user.get(0),user.get(1), "register");
             ClientOutputThread clientOutputThread = new ClientOutputThread(connection, registerRequest);
             Thread thread = new Thread(clientOutputThread);
@@ -47,14 +47,14 @@ public class Client {
     /**
      * Get user details for login or register
      * @return ArrayList<String> of user details
-     */
-    public ArrayList<String> getLoginOrRegisterDetails() throws IOException{
+     * */
+    public ArrayList<String> LoginOrRegisterInfo() throws IOException{
         String username;
         String password;
         ArrayList<String> user = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         while (!Thread.interrupted()) {
-            System.out.println("Enter your username: ");
+            System.out.println("Enter username: ");
             username = scanner.next();
             if (isValidUserNameOrPassword(username)) {
                 user.add(username);
@@ -65,7 +65,7 @@ public class Client {
         }
 
         while(!Thread.interrupted()) {
-            System.out.println("Enter your password: ");
+            System.out.println("Enter password: ");
             password = scanner.next();
             if (isValidUserNameOrPassword(password)) {
                 user.add(password);
@@ -82,20 +82,19 @@ public class Client {
      * @param argument command the user entered
      */
     public static void printInvalidUsernameOrPassword(String argument) {
-        System.out.println(argument + " must be at least 4 characters long");
+        System.out.println(argument + " must have at least 6 characters ");
     }
 
     /**
-     * Checks if the user entered a good username or password
-     * @param argument the value the user entered
+     * Checks if the user entered an available username or password
+     * @param argument the detail which the user entered
      * @return boolean, true if valid and false is if invalid
      */
     public static boolean isValidUserNameOrPassword(String argument) {
-        if(argument.length() >= 4) {
+        if(argument.length() >= 6) {
             return true;
         } else {
             return false;
         }
     }
 }
-

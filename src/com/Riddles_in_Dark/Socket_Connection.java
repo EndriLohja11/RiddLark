f3b7da45395937da8f97601f7d73603a0869173d
Nurgulll <93878260+Nurgulll@users.noreply.github.com>
2021-11-18 11:35:41 +0100

    Update and rename ServerSocketTask.java to Socket_Connection.java

--- src/com/Riddles_in_Dark/Socket_Connection.java
+++ src/com/Riddles_in_Dark/Socket_Connection.java
@@ -9,16 +9,16 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * This class represents a single thread which will be given to each client connected to the server at a particular time
- * All request and replies made between the client and server are handled here on the server side
+ * All call and responses made between the client and server are handled here on the server side
  */
-public class ServerSocketTask implements Runnable{
+public class Socket_Connection implements Runnable{
     private static ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
     private Socket connection;  // Create Socket
-    private ObjectInputStream clientRequest;
-    private ObjectOutputStream serverReply;
+    private ObjectInputStream clientCall;
+    private ObjectOutputStream serverResponse;
     private User user;
 
-    public ServerSocketTask(Socket s) {
+    public Socket_Connection(Socket s) {
         this.connection = s;
     }
 
@@ -26,46 +26,46 @@ public class ServerSocketTask implements Runnable{
     public void run() {
         try {
             while(true) {
-                clientRequest = new ObjectInputStream(connection.getInputStream()); //Create a Request Buffer
-                Request request = (Request) clientRequest.readObject(); //Read Client request, Convert it to String
-                System.out.println("Client sent : " + request.toString()); //Print the client request
-                handleRequest(request);
+                clientCall = new ObjectInputStream(connection.getInputStream()); //Create a call Buffer
+                Call call = (Call) clientCall.readObject(); //Read Client call, Convert it to String
+                System.out.println("Client sent : " + call.toString()); //Print the client call
+                handleCall(call);
             }
         } catch (IOException | ClassNotFoundException e) {
             System.out.println("connection closed");
 //                e.printStackTrace();
-            this.killSocketTask();
+            this.closeSocket();
         }
     }
 
     /**
-     * Kill this socket task if the user disconnects
+     * close the socket  if the user disconnects
      */
-    private void killSocketTask() {
+    private void closeSocket() {
         try {
-            if(clientRequest != null) clientRequest.close();
-            if(serverReply != null) serverReply.close();
+            if(clientCall != null) clientCall.close();
+            if(serverResponse != null) serverResponse.close();
             if(connection != null) connection.close();
         } catch (IOException e) {
-            System.out.println("Couldn't kill server task");
+            System.out.println("Couldn't close server");
 //            e.printStackTrace();
         }
 
     }
 
     /**
-     * Handles all request made from client to server
-     * @param request
+     * Handles all call made from client to server
+     * @param call
      * @throws IOException
      */
-    private void handleRequest(Request request) throws IOException {
-        String operation = request.getOperation();
+    private void handleCall(Call call) throws IOException {
+        String operation = call.getOperation();
         switch (operation) {
             case "register":
-                this.register(request);
+                this.register(call);
                 break;
             case "login":
-                this.login(request);
+                this.login(call);
                 break;
         }
     }
@@ -73,25 +73,25 @@ public class ServerSocketTask implements Runnable{
     /**
      * This function can be used to perform server side register user functionality
      */
-    private synchronized void register(Request request) throws IOException {
-        int userId = users.size() + 1;
-        String reqUsername = request.getUsername();
-        String reqPassword = request.getPassword();
+    private synchronized void register(Call call) throws IOException {
+
+        String username = call.getUsername();
+        String password = call.getPassword();
         boolean freeUserName = true;
         for(int i =  1; i <= users.size(); i++) {
-            if(users.isEmpty() || request == null) {
+            if(users.isEmpty() || call == null) {
                 break;
             }
             User _user = users.get(i);
             String userName = _user.getUsername();
-            if(userName.equals(reqUsername)) {
+            if(userName.equals(username)) {
                 freeUserName = false;
                 break;
             }
         }
         if (freeUserName) {
-            User user = new User(reqUsername, reqPassword, userId);
-            users.put(userId, user);
+            User user = new User(username, password);
+            
             this.notifyClient("Successfully registered", null, null, "login", connection);
         } else {
             this.notifyClient("The username is taken", null, null, "retry", connection);
@@ -101,17 +101,17 @@ public class ServerSocketTask implements Runnable{
     /**
      * This function can be used to perform server side login user functionality
      */
-    private synchronized void login(Request request) throws IOException {
-        String reqPassword = request.getPassword();
-        String reqUsername = request.getUsername();
+    private synchronized void login(Call call) throws IOException {
+        String password = call.getPassword();
+        String username = call.getUsername();
         for(int i =  1; i <= users.size(); i++) {
-            if(users.isEmpty() || request == null) {
+            if(users.isEmpty() || call == null) {
                 break;
             }
             User _user = users.get(i);
             String userPassword = _user.getPassword();
             String userName = _user.getUsername();
-            if(userName.equals(reqUsername) && userPassword.equals(reqPassword)) {
+            if(userName.equals(username) && userPassword.equals(password)) {
                 user = _user;
                 break;
             }
@@ -120,15 +120,15 @@ public class ServerSocketTask implements Runnable{
 
 
     /**
-     * Sends all replies from server to client
+     * Sends all responses from server to client
      * @param response
      */
     private void notifyClient(String response, User user, Object responseData, String nextOperation, Socket connection) {
         try {
-            Reply reply = new Reply(response, user, responseData, nextOperation);
-            serverReply = new ObjectOutputStream(connection.getOutputStream()); //Create a Reply Buffer
-            serverReply.writeObject(reply); //write "Reply" in the outputStream
-            serverReply.flush(); //Send written content to client
+            Response response1 = new Response(response, user, responseData, nextOperation);
+            serverResponse = new ObjectOutputStream(connection.getOutputStream()); //Create a Response Buffer
+            serverResponse.writeObject(response1); //write "Response" in the outputStream
+            serverResponse.flush(); //Send written content to client
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -136,5 +136,6 @@ public class ServerSocketTask implements Runnable{
 
 
     }
+
 
 
